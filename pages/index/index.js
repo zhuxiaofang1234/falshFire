e4f5b37eb0aedf17d70e704f1a284e999a6029e1
zhu <819508408@qq.com>
2019-07-10 17:32:24 +0800

    tab滑动

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,24 @@
-var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+var sliderWidth = 18; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
   data: {
     tabs: ["推荐", "关注"],
-    activeIndex: 0
+    activeIndex:0,
+    sliderOffset: 0,
+    sliderLeft: 0
   },
   onLoad: function () {
-   
+    //
+    this.setData({
+      sliderLeft:(36 - sliderWidth) / 2,
+      sliderOffset:122
+    });
     
   },
   tabClick: function (e) {
+    //console.log(e.currentTarget.offsetLeft);
     this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
       activeIndex: e.currentTarget.id
     });
   }
