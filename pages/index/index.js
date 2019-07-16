a8b42348d1b45a4d4b2db5dcce53b46d88426f0e
zhu <819508408@qq.com>
2019-07-16 22:45:30 +0800

    自定义导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,22 @@
 var sliderWidth = 18; // 需要设置slider的宽度，用于计算中间位置
+const App = getApp();
 
 Page({
   data: {
     tabs: ["推荐", "关注"],
     activeIndex:0,
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+    arrowShow:false
   },
-  onLoad: function () {
-    //
+  onLoad: function (options) {
+    var that = this;
+    that.setData({
+      navH: App.globalData.navHeight
+    });
+
+     
+    //设置滑块
     this.setData({
       sliderLeft:(36 - sliderWidth) / 2,
       sliderOffset:122
